d758fcf626fa78ccd76a215f23a338f3afb75333
Alejandro González Bueno <alejandro.gonzalez.bu@gmail.com>
2022-06-11 11:38:49 +0000

    macro

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -7,6 +7,13 @@
 
     {%- else -%}
 
+        {% if  target.name == 'dev' %} {{ custom_schema_name | trim }}
+        {% elif target.name == 'pre'  %} {{ custom_schema_name | trim }}  
+        {% elif target.name == 'pro'  %} {{ custom_schema_name | trim }}
+        {% elif target.name == 'user_dev'  %} {{ custom_schema_name | trim }}
+        {% else %} {{target.schema}}_{{ custom_schema_name | trim }}
+        {% endif %}
+
         {{ custom_schema_name | trim }}
 
     {%- endif -%}
